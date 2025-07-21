40e6d115eedab6811fe8a2c623289df9f5a3fbb5
PxS00 <lucas.rossonidd@gmail.com>
2025-07-21 19:46:12 -0300

    feat: add Header component and update App to use it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import './index.css';
+import Header from './components/Header/Header';
 
 const App = () => {
-  return <h1>Hello Tailwind</h1>
+  return <Header />
 }
 
 export default App
+
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return <div className="flex h-20 bg-gray-500">
+        <span>Olá Usuário</span>
+        <h1>Alura Newsletter</h1>
+        <input type="checkbox" />
+    </div>
+}
+
+export default Header
