ec7e0eb07d4fee687cdaddcc538292bf7d1a00d3
PxS00 <lucas.rossonidd@gmail.com>
2025-07-21 19:51:48 -0300

    style: enhance Header component with improved layout and styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
-    return <div className="flex h-20 bg-gray-500">
-        <span>Olá Usuário</span>
-        <h1>Alura Newsletter</h1>
-        <input type="checkbox" />
+    return <div className="flex h-20 bg-gray-500 justify-between items-center px-5 sm:rounded-xl sm: m-5">
+        <span className="text-gray-100">Olá Usuário</span>
+        <h1 className="text-gray-100 text-xl">Alura Newsletter</h1>
+        <input className="hidden sm:block" type="checkbox" />
     </div>
 }
 
