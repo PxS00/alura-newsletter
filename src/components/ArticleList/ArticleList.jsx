2237dd6d22691a9a3d2942d34f208ea3a7d8667f
PxS00 <lucas.rossonidd@gmail.com>
2025-07-23 12:34:20 -0300

    style: refactor components to use utility classes for consistent styling and improve layout; remove unused Input component

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -3,7 +3,7 @@ import Article from '../Article/Article'
 
 const ArticleList = () => {
   return (
-    <div className="mt-5 sm:mt-0 grid gap-5 m-auto max-w-2xl sm:grid-cols-2 lg:grid-cols-3 lg:max-w-[1000px]">
+    <div className="alura-articles-grid">
       {data.map((props, index) => (
         <Article key={index} {...props} />
       ))}
